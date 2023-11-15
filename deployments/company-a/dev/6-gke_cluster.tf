33ebf2c38a4cd6df598af4cac44b7ec476c9bc8e
omrimug <omrimug@gmail.com>
2023-11-15 15:35:49 +0200

    update node count

--- deployments/company-a/dev/6-gke_cluster.tf
+++ deployments/company-a/dev/6-gke_cluster.tf
@@ -5,9 +5,10 @@ module "dl-tf-gke-cluster" {
   location                      = local.cluster_location
   network_link                  = module.dl-tf-vpc.vpc_link
   subnetwork_link               = module.dl-tf-subnets.subnetwork_link
-  workload_identity_config      = "devops-v4.svc.id.goog"
+  workload_identity_config      = "dl-tf-405208.svc.id.goog"
   cluster_secondary_range_name  = "k8s-pod-range"
   services_secondary_range_name = "k8s-service-range"
   master_ipv4_cidr_block        = "172.16.0.0/28"
+  initial_node_count            = 1
 
 }
